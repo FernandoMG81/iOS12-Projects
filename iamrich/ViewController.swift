a08faf9003d182b2a0df0cf68c8adb80530663e0
Fernando Gordillo <fermg299@outlook.com>
2020-11-24 14:55:10 -0300

    Initial Commit

---
+++ iamrich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iamrich
+//
+//  Created by Fernando Martin Gordillo on 24/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
