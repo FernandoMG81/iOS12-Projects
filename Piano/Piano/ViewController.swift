442ea9f596261041c54e1860405f0b0dbc6a8ef7
Fernando Gordillo <fermg299@outlook.com>
2020-12-13 05:33:24 -0300

    Icono app

--- Piano/Piano/ViewController.swift
+++ Piano/Piano/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        decibelsLabel.transform =  CGAffineTransform(rotationAngle: CGFloat.pi / 2)
         // Do any additional setup after loading the view.
     }
 
