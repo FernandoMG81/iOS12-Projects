914170ab0fd859e25de05a14512edeeaa6acadbf
Fernando Gordillo <fermg299@outlook.com>
2021-02-18 01:27:44 -0300

    Diseño de la app

--- QuizApp/QuizApp/Controller/ViewController.swift
+++ QuizApp/QuizApp/Controller/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var labelQuestion: UILabel!
+    
+    @IBOutlet weak var labelQuestionNumber: UILabel!
+    
+    @IBOutlet weak var labelScore: UILabel!
+    
+    @IBOutlet weak var progressBar: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonPress(_ sender: UIButton) {
+        
+        
+    }
+    
+    //setear el color de la barra de estado
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return UIStatusBarStyle.lightContent
+    }
+    
+    //esconder la barra de estado
+    override var prefersStatusBarHidden: Bool{
+        return false
+    }
+    
 }
 
