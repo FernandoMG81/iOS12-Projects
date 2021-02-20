49b398bb10af62afe2aba1c26400eb7a3bd20abb
Fernando Gordillo <fermg299@outlook.com>
2021-02-19 22:14:42 -0300

    Generar las preguntas aleatorias

--- QuizApp/QuizApp/Controller/ViewController.swift
+++ QuizApp/QuizApp/Controller/ViewController.swift
@@ -18,19 +18,60 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var progressBar: UIView!
     
+    var currentScore = 0
+    var currentQuestionID = 0
+    var correctQuestionAnswered = 0
+    var currentQuestion : Question!
+    let factory = QuestionsFactory()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startGame()
      
+    }
+    
+    func startGame() {
+        currentScore = 0
+        currentQuestionID = 0
+        correctQuestionAnswered = 0
         
+        //Metodo para mezclar las preguntas
+        self.factory.questionsBank.questions.shuffle()
+        askNextQuestion()
+    }
+    
+    func askNextQuestion() {
+        if let newQuestion = factory.getQuestionAt(index: currentQuestionID){
+            self.currentQuestion = newQuestion
+            labelQuestion.text = self.currentQuestion.question
+            self.currentQuestionID+=1
+        }else{
+            gameOver()
+        }
         
-        
+    }
+    
+    func gameOver(){
+        //metodo para cuando no hay mas preguntas
     }
 
 
     @IBAction func buttonPress(_ sender: UIButton) {
-        let factory = QuestionsFactory()
-        let questions = factory.getRandomQuestion()
-        labelQuestion.text = "\(questions)"
+        var isCorrect : Bool
+        if sender.tag == 1{
+            //el usuario a cliqueado el verdadero
+            isCorrect = (self.currentQuestion.answer == true)
+        }else{
+            //el usuario a cliqueado el falso
+            isCorrect = (self.currentQuestion.answer == false)
+        }
+        
+        if isCorrect{
+            self.correctQuestionAnswered += 1
+        }
+        
+        askNextQuestion()
     }
     
     //setear el color de la barra de estado
